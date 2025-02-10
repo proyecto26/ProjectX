2190901630b0a5d7ae6e13a73ea2efd86b5d123d
jdnichollsc <jdnichollsc@hotmail.com>
2025-02-09 21:24:31 -0500

    improve organization for env variables

--- libs/backend/workflows/src/lib/worker.ts
+++ libs/backend/workflows/src/lib/worker.ts
@@ -5,6 +5,12 @@ import {
   WorkerOptions,
 } from '@temporalio/worker';
 
+/**
+ * Creates a worker options object for the Temporal worker.
+ * @param config - The NestJS configuration service.
+ * @param workflowsPath - The path to the workflows directory.
+ * @returns A Promise resolving to the worker options object.
+ */
 export async function createWorkerOptions(
   config: ConfigService,
   workflowsPath: string
